354d3dddb091a6b02d33e8ea9905292408043f5f
CrazyMax <crazy-max@users.noreply.github.com>
2020-04-29 11:17:22 +0200

    Use analysis ID as result link

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,13 @@ async function run() {
       throw new Error(`'files' input does not include valid file(s).`);
     }
 
+    core.info(`🏃 Running VirusTotal analysis...`);
     const vt = new VirusTotal(process.env.VT_API_KEY);
     files.forEach(filepath => {
-      core.info(`🏃 Running VirusTotal analysis of ${filepath}...`);
-      vt.upload(filepath).then(upres => {
-        vt.analysis(upres.data.data.id).then(anres => {
-          core.info(
-            `🐛 ${filepath} successfully uploaded. Check analysis status at https://www.virustotal.com/gui/file/${anres.data.meta.file_info.sha256}/detection`
-          );
-        });
+      vt.upload(filepath).then(res => {
+        core.info(
+          `🐛 ${filepath} successfully uploaded. Check detection analysis at https://www.virustotal.com/gui/file-analysis/${res.data.data.id}/detection`
+        );
       });
     });
   } catch (error) {
