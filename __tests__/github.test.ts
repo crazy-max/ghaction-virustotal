d394219eb8c330e80284f8d8402cb30027d5b637
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-07 16:35:52 +0200

    Fix tests

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -16,42 +16,38 @@ describe('github', () => {
   let release: githubm.Release;
   let assets: Array<githubm.ReleaseAsset>;
 
-  describe('getRelease', () => {
-    it('returns a GitHub release', async () => {
+  it('returns a GitHub release', async () => {
+    release = await githubm.getRelease(octokit, context);
+    console.log(release);
+    expect(release).not.toBeUndefined();
+    expect(release.id).toEqual(26057680);
+  });
+
+  it('returns a GitHub release assets list', async () => {
+    if (!release) {
       release = await githubm.getRelease(octokit, context);
-      console.log(release);
-      expect(release).not.toBeUndefined();
-      expect(release.id).toEqual(26057680);
-    });
+    }
+
+    assets = await githubm.getReleaseAssets(octokit, context, release, ['*.exe']);
+    console.log(assets);
+    expect(release).not.toBeUndefined();
+    expect(assets.length).toEqual(2);
+    expect(assets[0].name).toEqual('ghaction-virustotal-win32.exe');
   });
 
-  describe('getReleaseAssets', () => {
-    it('returns a GitHub release assets list', async () => {
-      if (!release) {
-        release = await githubm.getRelease(octokit, context);
-      }
-
+  it('download a GitHub release asset', async () => {
+    if (!release) {
+      release = await githubm.getRelease(octokit, context);
+    }
+    if (!assets) {
       assets = await githubm.getReleaseAssets(octokit, context, release, ['*.exe']);
-      console.log(assets);
-      expect(release).not.toBeUndefined();
-      expect(assets.length).toEqual(2);
-      expect(assets[0].name).toEqual('ghaction-virustotal-win32.exe');
-    });
-  });
+    }
 
-  describe('downloadReleaseAsset', () => {
-    it('download a GitHub release asset', async () => {
-      if (!release) {
-        release = await githubm.getRelease(octokit, context);
-      }
-      if (!assets) {
-        assets = await githubm.getReleaseAssets(octokit, context, release, ['*.exe']);
-      }
-
-      let {name, size, mime, file} = asset(await githubm.downloadReleaseAsset(octokit, context, assets[0], path.join(utilm.tmpDir(), assets[0].name)));
-      console.log(name, size, mime);
-      expect(size).toEqual(1306112);
-      expect(mime).toEqual('application/octet-stream');
-    });
+    const releaseAsset = await githubm.downloadReleaseAsset(octokit, context, assets[0], path.join(utilm.tmpDir(), assets[0].name));
+    console.log(releaseAsset);
+    const {name, size, mime, file} = asset(releaseAsset);
+    console.log(name, size, mime);
+    expect(size).toEqual(1306112);
+    expect(mime).toEqual('application/octet-stream');
   });
 });
--- __tests__/virustotal.test.ts
+++ __tests__/virustotal.test.ts
@@ -10,40 +10,34 @@ describe('virustotal', () => {
     });
   });
 
-  describe('asset', () => {
-    it('derives asset info from a path', async () => {
-      const {name, mime, size, file} = asset('tests/data/foo/bar.txt');
-      console.log(name, mime, size, file.toString());
-      expect(name).toEqual('bar.txt');
-      expect(mime).toEqual('text/plain');
-      expect(size).toEqual(7);
-      expect(file.toString()).toEqual('scan me');
+  it('derives asset info from a path', async () => {
+    const {name, mime, size, file} = asset('tests/data/foo/bar.txt');
+    console.log(name, mime, size, file.toString());
+    expect(name).toEqual('bar.txt');
+    expect(mime).toEqual('text/plain');
+    expect(size).toEqual(7);
+    expect(file.toString()).toEqual('scan me');
+  });
+
+  it('uploads asset on VirusTotal', async () => {
+    let vt: VirusTotal = new VirusTotal(process.env.VT_API_KEY || '');
+    await vt.upload('tests/data/foo/bar.txt').then(upload => {
+      console.log(upload);
+      expect(upload.id).not.toBeUndefined();
+      expect(upload.url).not.toBeUndefined();
     });
-  });
+  }, 30000);
 
-  describe('upload', () => {
-    it('uploads asset on VirusTotal', async () => {
-      let vt: VirusTotal = new VirusTotal(process.env.VT_API_KEY || '');
-      await vt.upload('tests/data/foo/bar.txt').then(upload => {
-        console.log(upload);
-        expect(upload.id).not.toBeUndefined();
-        expect(upload.url).not.toBeUndefined();
+  it('returns analysis info from VirusTotal', async () => {
+    let vt: VirusTotal = new VirusTotal(process.env.VT_API_KEY || '');
+    await vt.upload('tests/data/foo/bar.txt').then(upload => {
+      console.log(upload);
+      expect(upload.id).not.toBeUndefined();
+      expect(upload.url).not.toBeUndefined();
+      vt.analysis(upload.id).then(analysis => {
+        console.log(analysis);
+        expect(analysis.sha256).toEqual('cdf614b868c95c1367f3407bc43f9d74f10dfc0a96b2117808eb68569d6bb568');
       });
-    }, 30000);
-  });
-
-  describe('analysis', () => {
-    it('returns analysis info from VirusTotal', async () => {
-      let vt: VirusTotal = new VirusTotal(process.env.VT_API_KEY || '');
-      await vt.upload('tests/data/foo/bar.txt').then(upload => {
-        console.log(upload);
-        expect(upload.id).not.toBeUndefined();
-        expect(upload.url).not.toBeUndefined();
-        vt.analysis(upload.id).then(analysis => {
-          console.log(analysis);
-          expect(analysis.sha256).toEqual('cdf614b868c95c1367f3407bc43f9d74f10dfc0a96b2117808eb68569d6bb568');
-        });
-      });
-    }, 30000);
-  });
+    });
+  }, 30000);
 });
