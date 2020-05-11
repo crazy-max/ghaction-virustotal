3cb882d5f7b432cd2a02644a45a2616255a03ab6
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-11 19:36:11 +0200

    More tests

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -13,9 +13,21 @@ describe('util', () => {
     });
   });
 
-  describe('resolvePaths', () => {
-    it('resolves files given a set of paths', async () => {
-      expect(util.resolvePaths(['tests/data/**/*'])).toEqual(['tests/data/foo/bar.txt']);
-    });
+  it('resolves files given a set of paths', async () => {
+    expect(util.resolvePaths(['tests/data/**/*'])).toEqual(['tests/data/foo/bar.txt']);
+  });
+
+  it('loads context for this action', async () => {
+    process.env.GITHUB_REPOSITORY = 'crazy-max/ghaction-virustotal';
+    const context: util.Context = util.loadContext(process.env);
+    expect(context.github_owner).toEqual('crazy-max');
+    expect(context.github_repo).toEqual('ghaction-virustotal');
+  });
+
+  it('loads context for this action', async () => {
+    process.env.GITHUB_REPOSITORY = 'crazy-max/ghaction-virustotal';
+    const context: util.Context = util.loadContext(process.env);
+    expect(context.github_owner).toEqual('crazy-max');
+    expect(context.github_repo).toEqual('ghaction-virustotal');
   });
 });
