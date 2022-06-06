ea30b2cfd5b00e01cde6fee1ba47b4ffb7325058
xjin-splunk <xjin@splunk.com>
2022-06-06 16:35:51 -0700

    test simple large file url

--- src/virustotal.ts
+++ src/virustotal.ts
@@ -65,7 +65,7 @@ export class VirusTotal {
     const largeFileURL: string = await this.getURL();
 
     return this.instance
-      .post('https://www.virustotal.com/api/v3/files', fd.getBuffer(), {
+      .post('https://www.virustotal.com/_ah/upload/AMmfu6bZje23uFZ-eaMBPOlNYjYR9MGQDIzxaXq-jQizeeVtYqDir1Cd55_qjrirSfaNhc0b6R4EqURy8UBg_gEPHQCmyd5WVhOvFuBqMUapouq9qyUpTfyOVFX_Q48Zmpe4oK9YEM9xKJ0ed3pOi0RgQ7R53gTrMc4LQGaLm5YFoi1gcvpWG5rydexRd-QVrpw0oalSwZp6a3Q9ks1-xLWMcAxsnljVgT1VMe0c6hYkqmIP_nXMPXUMm9NjSU-EtKldspFhuDrN_2qdTsSEwhEK9Jywb9L-vwtByi3T76D1jHA-AEtnReVaW8TiwK_Rq93AdxuLbxQt7aVVou0GlcQLhARvQuaXH3EE7MK4qZR9dEdlZiDCezHOdcuBp2BYJbVfxsxq8lohqp-Pmy7c5njkHU3mGkE67Rb_zdveXpAAUvG8X_ZqvVJR-NKbZpQ9QVCcmPeKG4kLwd4ywca2N4PQpMF_-D9MdXWVSeAnuOOmnjZvKYFT1JGKiZPLQ_2hpdjEp_tDF6EmXZ1I4TmW1k-QE22nwKgmSXqa6_LGX6SQ0utvMl6FC0jb6_SjlHZ2gK4QTHMoTLYyCkkgZGYRYCdDqpCrxVuNRvUqkg0l-DFMScrDtFkDvpM3DWmmT_mKMWVjPzlPEvH12Cg9nultNsoIDFGsnk-fSid_TtHn0Qq11ZzR6rCrXIV5t9wzm9-_0IXQV-rxYLIDSTUABf__Nw203sF-Jmjm7xexJyWnyi0Pbs3vLlpsw_5m1usmZxWbjPXqKC_RupJ7ysKCJ4hD58lJcE4kwLFuFBqONL6MFOiLbODNWjcJJ5K4zTWEddjD_NfJb9KwLGmlv30A0GWT6yZvmC2NH4Wbuq3XiW-GV1MNjk97crVEGr-o7cwkC2hAcBn1cmWkEjofH-iEFQUMi428z_HqDbMzDk4zLA-l73_NuX6HD42MzeQvyTDNTH8GLhiVifdpE7WRDvm0eM6bwQ1pKZCSdpBwFvQlFZmBjEBoZpKJIuS0VHOKgFkzoxpQlWExPEZ8gLHxYUYF8Mukirnt4dwcZS-IdA/ALBNUaYAAAAAYp6RgWu1_MAfWlyqItwBMgYHFICntB2L/', fd.getBuffer(), {
       // .post(largeFileURL, fd.getBuffer(), {
         headers: fd.getHeaders()
       })
