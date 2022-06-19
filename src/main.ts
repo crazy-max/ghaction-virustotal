8b7a04f83b631b5d9323587171688b07ededa823
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-06-19 22:23:54 +0200

    Set collapsible assets in release body (#128)

--- src/main.ts
+++ src/main.ts
@@ -62,12 +62,17 @@ async function runForReleaseEvent(vt: VirusTotal) {
   core.info(`Release event detected for ${github.context().ref} in this workflow. Preparing to scan assets...`);
 
   const release = await github.getRelease(octokit, github.context().ref.replace('refs/tags/', ''));
-  release.body = release.body.concat(`\n\n🛡 [VirusTotal GitHub Action](https://github.com/crazy-max/ghaction-virustotal) analysis:`);
 
   const assets = await github.getReleaseAssets(octokit, release, inputs.files);
   if (assets.length == 0) {
     core.warning(`No assets were found for ${release.tag_name} release tag. Please check the 'files' input.`);
     return;
+  }
+
+  if (assets.length <= 5) {
+    release.body = release.body.concat(`\n\n🛡 [VirusTotal GitHub Action](https://github.com/crazy-max/ghaction-virustotal) analysis:`);
+  } else {
+    release.body = release.body.concat(`\n\n<details>\n  <summary>🛡 VirusTotal analysis</summary>\n`);
   }
 
   core.info(`${assets.length} asset(s) will be sent to VirusTotal for analysis.`);
@@ -78,17 +83,20 @@ async function runForReleaseEvent(vt: VirusTotal) {
         await vt.monitorItems(await github.downloadReleaseAsset(octokit, asset, path.join(context.tmpDir(), asset.name)), inputs.monitorPath).then(upload => {
           outputAnalysis.push(`${asset.name}=${upload.url}`);
           core.info(`${asset.name} successfully uploaded. Check detection analysis at ${upload.url}`);
-          release.body = release.body.concat(`\n* [\`${asset.name}\`](${upload.url})`);
+          release.body = release.body.concat(`\n  * [\`${asset.name}\`](${upload.url})`);
         });
       } else {
         await vt.files(await github.downloadReleaseAsset(octokit, asset, path.join(context.tmpDir(), asset.name))).then(upload => {
           outputAnalysis.push(`${asset.name}=${upload.url}`);
           core.info(`${asset.name} successfully uploaded. Check detection analysis at ${upload.url}`);
-          release.body = release.body.concat(`\n* [\`${asset.name}\`](${upload.url})`);
+          release.body = release.body.concat(`\n  * [\`${asset.name}\`](${upload.url})`);
         });
       }
     });
   });
+  if (assets.length > 5) {
+    release.body = release.body.concat(`\n<sub>These scans were produced by the [VirusTotal GitHub Action](https://github.com/crazy-max/ghaction-virustotal)</sub></details>\n`);
+  }
 
   if (/true/i.test(core.getInput('update_release_body'))) {
     core.debug(`Appending analysis link(s) to release body`);
